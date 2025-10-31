092b6f2f61dd6662a47419be9409a856f4ecfc5a
Zinneerah <138329715+Zinneerah@users.noreply.github.com>
2025-10-31 21:21:03 +0500

    Delete cypress directory

--- cypress/integration/login_failure.js
+++
@@ -1,5 +0,0 @@
-describe('Login Failure Test', () => {
-  it('should show error for invalid login', () => {
-    cy.loginInvalid('invaliduser', 'wrongpassword', 'Username and password do not match')
-  })
-})
--- cypress/integration/login_success.js
+++
@@ -1,8 +0,0 @@
-describe('Login Success Test', () => {
-  it('should login successfully', () => {
-    cy.login('standard_user', 'secret_sauce')
-    cy.url().should('include', '/inventory.html')
-    cy.get('.title').should('contain', 'Products')
-    cy.logout()
-  })
-})
